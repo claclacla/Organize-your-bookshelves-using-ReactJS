777990f775f3f7b376e34c119e18e76684c7121e
Adelchino Simone <a.simone@wwg.it>
2017-09-17 20:34:37 +0200

    ListBooks > Added API call for retreiving the books list

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class BookShelf extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    shelfName: PropTypes.string.isRequired
+    books: PropTypes.array.isRequired
   };
 
   render() {
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import * as BooksAPI from './BooksAPI';
 import BookShelf from './BookShelf';
 
 class ListBooks extends Component {
+  CURRENTLY_READING = "currentlyReading";
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    })
+  }
+
   render() {
     return (
       <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-      <div className="list-books-content">
-        <div>
-          <BookShelf title="Currently reading" shelfName="currentlyReading"/>
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf title="Currently reading" books={this.state.books.filter(book => book.shelf === this.CURRENTLY_READING)} />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
-      <div className="open-search">
-        <Link to="/search">Add a book</Link>
-      </div>
-    </div>
     );
   }
 }
