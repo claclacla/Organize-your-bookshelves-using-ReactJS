0b4940a21c97aaceb37ab543e3991a6189cebbfa
Adelchino Simone <a.simone@wwg.it>
2017-09-24 20:01:46 +0200

    Added Queue class for multi async requests queue. SearchBooksText > Added queue property for search requests

--- src/SearchBooksText.js
+++ src/SearchBooksText.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Queue from './Queue';
+
 class SearchBooksText extends Component {
+  constructor() {
+    super();
+
+    this.queue = new Queue();
+  }
+
   static propTypes = {
     search: PropTypes.func.isRequired
   }
@@ -12,14 +20,20 @@ class SearchBooksText extends Component {
 
   setSearchText = (event) => {
     var searchText = event.target.value;
-
     this.setState({ searchText });
-    this.props.search(searchText);
+
+    this.queue.push(() => {
+      return new Promise((resolve, reject) => {
+        this.props.search(searchText).then((res) => {
+          resolve(res);
+        });
+      });
+    });
   }
 
   render() {
     return (
-      <input type="text" onChange={this.setSearchText} value={this.state.searchText} placeholder="Search by title or author"/>
+      <input type="text" onChange={this.setSearchText} value={this.state.searchText} placeholder="Search by title or author" />
     );
   }
 }
