012fac7b3041c1ba7c3f4a01cf8834ba3e029df7
Adelchino Simone <a.simone@wwg.it>
2017-09-27 22:46:51 +0200

    Book rewritten using older version

--- src/Book.js
+++ src/Book.js
@@ -1,17 +1,20 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-=======
-import React from 'react';
->>>>>>> master
 import PropTypes from 'prop-types';
 
-import { Link } from 'react-router-dom';
+// https://facebook.github.io/react/docs/lifting-state-up.html
+// https://facebook.github.io/react/docs/lifting-state-up.html#lifting-state-up
 
-const Book = function (props) {
-  const { book, setBookShelf } = props;
+class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    setBookShelf: PropTypes.func.isRequired
+  }
 
-<<<<<<< HEAD
+  setBookShelf = (event) => {
+    this.props.setBookShelf(this.props.book, event.target.value);
+  }
+
   render() {
     const { book } = this.props;
 
@@ -23,23 +26,6 @@ const Book = function (props) {
 
     /*
             <select value={bookShelf} onChange={this.setBookShelf}>
-=======
-  var bookShelf = book.shelf;
-
-  if (bookShelf === undefined) {
-    bookShelf = "none";
-  }
-
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <Link to={"/book/" + book.id}>
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
-          </Link>
-          <div className="book-shelf-changer">
-            <select value={bookShelf} onChange={(event) => { setBookShelf(book, event.target.value) }}>
->>>>>>> master
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -59,22 +45,9 @@ const Book = function (props) {
           <div className="book-title">{book.title}</div>
           <div className="book-authors">{(Array.isArray(book.authors)) && book.authors.map((author, idx) => <div key={idx}>{author}</div>)}</div>
         </div>
-<<<<<<< HEAD
       </li>
     );
   }
-=======
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{(Array.isArray(book.authors)) && book.authors.map((author, idx) => <div key={idx}>{author}</div>)}</div>
-      </div>
-    </li>
-  );
-}
-
-Book.propTypes = {
-  book: PropTypes.object.isRequired,
-  setBookShelf: PropTypes.func.isRequired
->>>>>>> master
 }
 
 export default Book
