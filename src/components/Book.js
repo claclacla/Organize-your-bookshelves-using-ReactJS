7f69a75da6b5d97c212b9047bef2c4491950e44d
Adelchino Simone <a.simone@wwg.it>
2017-09-29 23:56:26 +0200

    Book > Bugfix > props was still used a class property

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,32 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Book = function(props) {
-  render() {
-    const { book } = this.props;
+const Book = function (props) {
+  const { book } = props;
 
-    var bookShelf = book.shelf;
+  var bookShelf = book.shelf;
 
-    if (bookShelf === undefined) {
-      bookShelf = "none";
-    }
-
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <Link to={"/book/" + book.id}>
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
-            </Link>
-          </div>
-          <Link to={"/pick-book-shelf?bookId=" + book.id + "&bookShelf=" + book.shelf}><div className="book-shelf-changer"></div></Link>
-          <div className="book-shelf">{bookShelf}</div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{(Array.isArray(book.authors)) && book.authors.map((author, idx) => <div key={idx}>{author}</div>)}</div>
-        </div>
-      </li>
-    );
+  if (bookShelf === undefined) {
+    bookShelf = "none";
   }
+
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <Link to={"/book/" + book.id}>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
+          </Link>
+        </div>
+        <Link to={"/pick-book-shelf?bookId=" + book.id + "&bookShelf=" + book.shelf}><div className="book-shelf-changer"></div></Link>
+        <div className="book-shelf">{bookShelf}</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{(Array.isArray(book.authors)) && book.authors.map((author, idx) => <div key={idx}>{author}</div>)}</div>
+      </div>
+    </li>
+  );
 }
 
 Book.propTypes = {
