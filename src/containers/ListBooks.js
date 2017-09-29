bdf843e06a27fa76e6ea51b904992a1bdf5e425f
Adelchino Simone <a.simone@wwg.it>
2017-09-30 00:01:54 +0200

    ListBooks > Rewritten as functional stateless component

--- src/containers/ListBooks.js
+++ src/containers/ListBooks.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Data from '../Data';
 import BookShelf from '../components/BookShelf';
 
-class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired
-  }
+const ListBooks = function (props) {
+  const { books } = props;
 
-  render() {
-    const { books } = this.props;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <BookShelf title="Currently reading" books={books.filter(book => book.shelf === Data.currentlyReading.value)}/>
-            <BookShelf title="Want to read" books={books.filter(book => book.shelf === Data.wantToRead.value)}/>
-            <BookShelf title="Read" books={books.filter(book => book.shelf === Data.read.value)}/>
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <BookShelf title="Currently reading" books={books.filter(book => book.shelf === Data.currentlyReading.value)} />
+          <BookShelf title="Want to read" books={books.filter(book => book.shelf === Data.wantToRead.value)} />
+          <BookShelf title="Read" books={books.filter(book => book.shelf === Data.read.value)} />
         </div>
       </div>
-    );
-  }
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
+}
+
+ListBooks.propTypes = {
+  books: PropTypes.array.isRequired
 }
 
 export default ListBooks
