1de794a6e8ecd98ecde42f2b6b5a8774057f199d
Adelchino Simone <a.simone@wwg.it>
2017-10-02 22:36:09 +0200

    Book rewritten as stateless function

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import BookShelfChanger from './BookShelfChanger';
 
-class Book extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired
+const Book = function (props) {
+  const { book } = props;
+
+  var bookShelf = book.shelf;
+
+  if (bookShelf === undefined) {
+    bookShelf = "none";
   }
 
-  render() {
-    const { book } = this.props;
-
-    var bookShelf = book.shelf;
-
-    if (bookShelf === undefined) {
-      bookShelf = "none";
-    }
-
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <Link to={"/book/" + book.id}>
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
-            </Link>
-          </div>
-          <div className="book-shelf-changer-container">
-            <div className="inline">{bookShelf}</div>
-            <BookShelfChanger book={book} />
-          </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{(Array.isArray(book.authors)) && book.authors.map((author, idx) => <div key={idx}>{author}</div>)}</div>
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <Link to={"/book/" + book.id}>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
+          </Link>
         </div>
-      </li>
-    );
-  }
+        <div className="book-shelf-changer-container">
+          <div className="inline">{bookShelf}</div>
+          <BookShelfChanger book={book} />
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{(Array.isArray(book.authors)) && book.authors.map((author, idx) => <div key={idx}>{author}</div>)}</div>
+      </div>
+    </li>
+  );
+}
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired
 }
 
 export default Book
